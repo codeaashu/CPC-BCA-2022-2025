6ef2f2718be8731d37ed7f270ecd8dec1a533b36
Sharwan Kumar <shrawan.bcastudent22.16780@cimage.in>
2025-07-15 17:28:28 +0530

    Sharwan Kumar id:- 16780

---
+++ 16780/admin_panel/partials/select_classes.php
@@ -0,0 +1,22 @@
+
+<option value="" selected>Select Class</option>
+  <option value="12m">12 (Math)</option>
+<option value="12b">12 (Bio)</option>
+<option value="12c">12 (Commerce)</option>
+<option value="11m">11 (Math)</option>
+<option value="11b">11 (Bio)</option>
+<option value="11c">11 (Commerce)</option>
+<option value="10">10</option>
+<option value="9">9</option>
+<option value="8">8</option>
+
+<option value="7">7</option>
+<option value="6">6</option>
+<option value="5">5</option>
+<option value="4">4</option>
+<option value="3">3</option>
+<option value="2">2</option>
+<option value="1">1</option>
+<option value="pg">Nursery</option>
+<option value="lkg">lkg</option>
+<option value="ukg">ukg</option>
