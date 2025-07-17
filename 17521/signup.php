bb67f2cf456d521c0d15c20a28f29a0b774c6f48
Ashutosh Kumar <130897584+codeaashu@users.noreply.github.com>
2025-07-17 20:49:10 +0530

    Aryan Kumar - ID:- 17521

--- 17521/signup.php
+++ 17521/signup.php
@@ -164,7 +164,7 @@ error:function (){}
 		});
 	</script>
 <!--===============================================================================================-->
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAKFWBqlKAGCeS1rMVoaNlwyayu0e0YRes"></script>
+	
 	<script src="js/map-custom.js"></script>
 <!--===============================================================================================-->
 	<script src="js/main.js"></script>
